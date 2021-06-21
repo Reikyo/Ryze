8ccc5638525f3b382b211097059ee1524a880905
Darren Temple <darren.temple.101@gmail.com>
2021-06-21 12:25:37 +0100

    - Added player move limit as dictated by the camera viewport

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Camera camMainCamera;
+
     public float fMetresPerSecMove = 100f;
     public float fMetresPerSecMoveDeltaBoost = 20f;
+    private float fPosYDeltaCamPlayer;
     private float fInputHorz;
     private float fInputVert;
     private Vector3 v3DirectionMove;
+    private Vector3 v3CamLowerLeft;
+    private Vector3 v3CamUpperRight;
+    private Vector3 v3MoveLimitLowerLeft;
+    private Vector3 v3MoveLimitUpperRight;
+    private Vector3 v3MoveLimitCamOffset = new Vector3(5f, 0f, 5f);
 
     // ------------------------------------------------------------------------------------------------
 
     void Start()
     {
-
+        camMainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        fPosYDeltaCamPlayer = camMainCamera.transform.position.y - transform.position.y;
+        v3CamLowerLeft = camMainCamera.ViewportToWorldPoint(new Vector3(0, 0, fPosYDeltaCamPlayer));
+        v3CamUpperRight = camMainCamera.ViewportToWorldPoint(new Vector3(1, 1, fPosYDeltaCamPlayer));
+        v3MoveLimitLowerLeft = v3CamLowerLeft + v3MoveLimitCamOffset;
+        v3MoveLimitUpperRight = v3CamUpperRight - v3MoveLimitCamOffset;
     }
 
     // ------------------------------------------------------------------------------------------------
@@ -24,6 +37,17 @@ public class PlayerController : MonoBehaviour
     {
         fInputHorz = Input.GetAxis("Horizontal");
         fInputVert = Input.GetAxis("Vertical");
+
+        if (    ((fInputHorz < 0) && (transform.position.x <= v3MoveLimitLowerLeft.x))
+            ||  ((fInputHorz > 0) && (transform.position.x >= v3MoveLimitUpperRight.x)) )
+        {
+            fInputHorz = 0f;
+        }
+        if (    ((fInputVert < 0) && (transform.position.z <= v3MoveLimitLowerLeft.z))
+            ||  ((fInputVert > 0) && (transform.position.z >= v3MoveLimitUpperRight.z)) )
+        {
+            fInputVert = 0f;
+        }
 
         if (Math.Abs(fInputHorz) + Math.Abs(fInputVert) > 0f)
         {
