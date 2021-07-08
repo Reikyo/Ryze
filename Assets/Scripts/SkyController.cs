450d2dd4b3954d452c73593134f48c485f4371c2
reikyo <darren.temple.101@gmail.com>
2021-07-08 22:06:56 +0100

    - Changed sky-box for sky-plane, and added a controller code to give the appearance of player movement through space

---
+++ Assets/Scripts/SkyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+    public float fMetresPerSecMove = 10f;
+
+    // ------------------------------------------------------------------------------------------------
+
+    void Update()
+    {
+        transform.Translate(fMetresPerSecMove * Time.deltaTime * -Vector3.forward);
+
+        if (transform.position.z <= -4000f)
+        {
+            transform.Translate(8000f * Vector3.forward);
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------
+
+}
